eede885cfc3b2c72c2801e7abc4a21227c9495f4
grossjw58 <grossjw58@yahoo.com>
2024-08-26 18:28:38 -0400

    added asteroids and colission detection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 import player as pl
+import asteroid 
+import asteroidfield
+import sys
 
 updateable = pygame.sprite.Group()
 
@@ -9,21 +12,34 @@ def main() -> None:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock: pygame.time.Clock = pygame.time.Clock()
     dt: float = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    pl.Player.containers = (updateable, drawable)
+    asteroid.Asteroid.containers = (updateable, drawable, asteroids)
+    asteroidfield.AsteroidField.containers = updateable
+
     player: pl.Player = pl.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field: asteroidfield.AsteroidField = asteroidfield.AsteroidField()
 
-    updateable = pygame.sprite.Group(player)
-    drawable = pygame.sprite.Group(player)
-
+    print(len(updateable))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        
-        screen.fill("black")
-
+                sys.exit()
+                
         for sprite in updateable:
-            sprite.update(dt)
+            sprite.update(dt)    
+
+        for roid in asteroids:
+            if roid.is_colliding(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
         
         for sprite in drawable:
             sprite.draw(screen)
--- player.py
+++ player.py
@@ -18,7 +18,7 @@ class Player(circleshape.CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white",self.triangle(), 2)
+        pygame.draw.polygon(screen,"white",self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
